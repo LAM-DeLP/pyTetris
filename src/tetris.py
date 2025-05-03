ee3aaceedcaf580c24168b10bd6220890f1e11e7
LAM-DeLP <de.lamnp42@@gmail.com>
2025-05-03 14:51:35 +0900

    fixed?

--- src/tetris.py
+++ src/tetris.py
@@ -8,8 +8,10 @@ class Mino:
     def __init__(self,numOfmino):
         self.minoVer = [[0,0]]
         self.times = numOfmino
+        self.lifetime = 100
 
     def self_generate(self):
+        self.minoVer = [[0,0]]
         for i in range(self.times):
             tempVer = []
             for j in range(4):                
@@ -24,59 +26,100 @@ class Mino:
     def rotate(self):
         for i in range(len(self.minoVer)):
             self.minoVer[i] = [-self.minoVer[i][1],self.minoVer[i][0]]
+    
+    def decrease_lifetime(self):
+        self.lifetime -= 20
+    
+    def reset_lifetime(self):
+        self.lifetime = 100
 
-    def get_position(self):
+    def get_shape(self):
         return self.minoVer
+    def get_lifetime(self):
+        return self.lifetime
 
 class Board:
-    def __init__(self,length,height):
+    def __init__(self,length,height,ix,iy):
         self.grid = numpy.zeros((height,length))
         self.blockdata = numpy.zeros((height,length))
         self.clearStandard = length
+        self.minocoord = [ix,iy]
+        self.minospeed = [0,0]
 
-    def assign(self,positions,x,y):
-        
-        for dx,dy in positions:
-            if x+dx < 0 or x+dx > len(self.grid[0]) or y+dy < 0 or y+dy > len(self.grid):
-                self.brockdata = self.grid.copy()
-                print("---------------------")
-                print(self.blockdata)
-                raise Exception("Invalid position")
-            else:
-                self.grid[y+dy][x+dx] += 1
-
-    def is_validmove(self,positions,x,y):
-        for dx,dy in positions:
-            if x+dx < 0 or x+dx > len(self.grid[0]) or y+dy < 0 or y+dy > len(self.grid):
+    def x_validmove(self,shape,dx):
+        for rx,ry in shape:
+            x = self.minocoord[0]+rx+dx
+            y = self.minocoord[1]+ry
+            if x<0 or x>=len(self.grid[0]):
+                self.minospeed[0] = 0
                 return False
+            elif self.blockdata[y][x]==1:
+                self.minospeed[0] = 0
+                return False
+        self.minospeed[0] = dx  
+        return True          
+        
+    def y_validmove(self,shape,dy):
+        for rx,ry in shape:
+            x = self.minocoord[0]+rx
+            y = self.minocoord[1]+ry+dy
+            if y<0 or y>=len(self.grid):
+                self.minospeed[1] = 0
+                return False
+            elif self.blockdata[y][x]==1:
+                self.minospeed[1] = 0
+                return False
+        self.minospeed[1] = dy
         return True
-                
-            
-            
+        
+    def set_mino(self,shape):
+        self.grid = numpy.copy(self.blockdata)
+        print(self.minospeed)
+        self.minocoord[0]+=self.minospeed[0]
+        self.minocoord[1]+=self.minospeed[1]
+        for rx,ry in shape:
+            x=self.minocoord[0]+rx
+            y=self.minocoord[1]+ry
+            self.grid[y][x] = 1
+
+    def is_fixed(self,lifetime):
+        if lifetime<=0:
+            self.blockdata = numpy.copy(self.grid)
+            self.minocoord = [4,4]
+            return False
+        else:
+            return True
+        
+
+    def get_minostate(self):
+        pass
 
 class Game:
     def __init__(self):
-        self.board = Board(10,20)
+        self.board = Board(10,20,4,4)
         self.mino = Mino(3)
         self.mino.self_generate()
-
-
+    
     def update(self):
-        positions = self.mino.get_position()
-        self.board.is_validmove(positions,3,10)
+        while True:
+            dy=1
+            dx=random.choice([-1,0,1])
+            shape = self.mino.get_shape()
+            lifetime = self.mino.get_lifetime()
+            self.board.x_validmove(shape,dx)
+            if self.board.y_validmove(shape,dy) == False:
+                self.mino.decrease_lifetime()
+            if self.board.is_fixed(lifetime) == False:
+                self.mino.self_generate()
+                self.mino.reset_lifetime()
+            self.board.set_mino(shape)
 
-        
-
-
+            print(self.board.grid)
+            time.sleep(0.25)
+            
 
 
 if __name__ == '__main__':
     game1 = Game()
     game1.update()
-
-
-
     #回転も移動も何か返して妥当性を判定する
-
-    
-
