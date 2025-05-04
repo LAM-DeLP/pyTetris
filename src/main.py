4143ada7d5f526937bea941762fbfeed99b83059
LAM-DeLP <de.lamnp42@@gmail.com>
2025-05-04 13:39:09 +0900

    add fallspan

--- src/main.py
+++ src/main.py
@@ -5,7 +5,18 @@ import tetris
 tetrisApp = tetris.Game()
 
 window = pyglet.window.Window(width=540,height=720)
+
 pyglet.gl.glClearColor(0.3, 0.8, 0.5, 1.0)
+
+@window.event
+def on_key_press(symbols, modifiers):
+    if symbols == pyglet.window.key.LEFT:
+        tetrisApp.dx = -1
+    if symbols == pyglet.window.key.RIGHT:
+        tetrisApp.dx = 1
+    if symbols == pyglet.window.key.DOWN:
+        print("down")
+        tetrisApp.dy = 1
 
 @window.event
 def on_draw():
@@ -14,12 +25,12 @@ def on_draw():
     square = shapes.Rectangle(20, 20, 320, 640, color=(0, 0, 0), batch=batch)
     rectangles = []
     tetrisApp.update()
-
     for y,row in enumerate(reversed(tetrisApp.board.grid)):
         for x,grid in enumerate(row):
             if grid == 1:
                 rect = shapes.Rectangle(20+x*32, 20+y*32, 31, 31, color=(255, 255, 255), batch=batch)
                 rectangles.append(rect)
+    
     batch.draw()
-
+# 0.5秒ごとに update を呼び出す
 pyglet.app.run()
--- src/tetris.py
+++ src/tetris.py
@@ -1,6 +1,5 @@
 import random
 import numpy
-import time
 class Mino:
     velo = [[1,0],[-1,0],[0,1],[0,-1]]
     def __init__(self,numOfmino):
@@ -25,8 +24,8 @@ class Mino:
         for i in range(len(self.minoVer)):
             self.minoVer[i] = [-self.minoVer[i][1],self.minoVer[i][0]]
     
-    def decrease_lifetime(self):
-        self.lifetime -= 20
+    def decrease_lifetime(self,dt):
+        self.lifetime -= dt
     
     def reset_lifetime(self):
         self.lifetime = 100
@@ -51,12 +50,9 @@ class Board:
             x = self.minocoord[0]+rx+dx
             y = self.minocoord[1]+ry
             if x<0 or x>=len(self.grid[0]):
-                self.minocoord[0] += 0
                 return False
             elif self.blockdata[y][x]==1:
-                self.minocoord[0] += 0
                 return False
-        self.minocoord[0] += dx  
         return True          
         
     def y_validmove(self,shape,dy):
@@ -64,12 +60,9 @@ class Board:
             x = self.minocoord[0]+rx
             y = self.minocoord[1]+ry+dy
             if y<0 or y>=len(self.grid):
-                self.minocoord[1] -= 0
                 return False
             elif self.blockdata[y][x]==1:
-                self.minocoord[1] -= 0
                 return False
-        self.minocoord[1] += dy
         return True
         
     def set_mino(self,shape):
@@ -85,15 +78,15 @@ class Board:
             self.minocoord = [self.initcoord[0],self.initcoord[1]]
             for y,row in enumerate(self.blockdata):
                 if numpy.sum(self.blockdata[y])==10:
-                    print("消去")
                     self.blockdata =numpy.delete(self.blockdata,y,axis=0)
-                    print(self.blockdata)
-                    self.blockdata =numpy.insert(self.blockdata,0,[0,0,0,0,0,0,0,0,0,0],axis=0)
-
-
+                    self.blockdata =numpy.insert(self.blockdata,0,numpy.zeros((10,1)),axis=0)
             return False
         else:
             return True
+
+    def move_mino(self,dx,dy):
+        self.minocoord[0] += dx
+        self.minocoord[1] += dy
 
     def get_minostate(self):
         pass
@@ -103,21 +96,47 @@ class Game:
         self.board = Board(10,20,5,3)
         self.mino = Mino(3)
         self.mino.self_generate()
-    
+        self.flametime = 0
+        self.dx,self.dy = 0,0
+
+    def flamemanager(self,dx,dy,dt):
+        if self.flametime > 100:
+            self.board.move_mino(dx,dy)
+            self.flametime = 0
+        else:
+            self.flametime += dt
+
     def update(self):
-        dy=1
-        dx=random.choice([-1,0,1])
+
         shape = self.mino.get_shape()
         lifetime = self.mino.get_lifetime()
 
-        if self.board.y_validmove(shape,dy) == False:
-            self.mino.decrease_lifetime()
+
+                
+        if self.board.y_validmove(shape,1) == False:
+            self.mino.decrease_lifetime(2)
             if self.board.is_fixed(lifetime) == False:
                 self.mino.self_generate()
                 self.mino.reset_lifetime()
+        else:
+            self.flamemanager(0,1,10)
 
-        self.board.x_validmove(shape,dx)
+        if self.board.y_validmove(shape,self.dy) == False:
+            self.mino.decrease_lifetime(2)
+            if self.board.is_fixed(lifetime) == False:
+                self.mino.self_generate()
+                self.mino.reset_lifetime()
+        else:
+            self.board.move_mino(0,self.dy)
+
+        if self.board.x_validmove(shape,self.dx)==False:
+            pass
+        else:
+            self.board.move_mino(self.dx,0)
+
         self.board.set_mino(shape)
+        self.dx,self.dy = 0,0
+
 if __name__ == '__main__':
     game1 = Game()
     game1.update()
